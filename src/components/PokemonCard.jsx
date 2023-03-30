9346a89444d5d403f1154fd3e1a0bdc14490bb94
JulienBenoit7 <julien-benoit@hotmail.fr>
2023-03-30 10:21:47 +0200

    exo react 5

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,24 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
   const pokemon = props.thePokemonProp;
 
-    return (
-      <div>
-        <figure>
-         {pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} alt={pokemon.name}/>}
-    <figcaption>{pokemon.name}</figcaption>
-</figure>
-</div>
+  return (
+    <>
+      <figure>
+        {pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} alt={pokemon.name} />}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    </>
   )
-  ;
+    ;
 }
 
-
+PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
+}
 
 export default PokemonCard;
-    
